f405419b0c163169e9a4b1968b7743068cdb087d
tomek.larysz <ttomek.larysz@gmail.com>
2023-10-06 16:34:13 +0200

    Added scoreboard

--- script.js
+++ script.js
@@ -1,63 +1,98 @@
+const choice = document.querySelector('#choice');
+
 function getComputerChoice() {
-    let x = Math.floor(Math.random() * 3);
-    // console.log(x);
+    let x = Math.floor(Math.random() * 3); // random number 0, 1 or 2
+    let computerChoice = '';
+    console.log(x);
     switch(x) {
         case 0:
-            return "Rock";
+            computerChoice = 'Rock';
         case 1:
-            return "Paper";
+            computerChoice = 'Paper';
         case 2:
-            return "Scissors";
-        default:
-            return "Something went wrong";
+            computerChoice = 'Scissors';
     }
+    choice.textContent = `Computer is ${computerChoice}`;
+    return computerChoice;
 }
 
-// console.log(getComputerChoice());
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+
+let playerPoints = 0;
+let computerPoints = 0;
+
+function update() {
+    player.textContent = `You: ${playerPoints.toString()}`;
+    computer.textContent = `Computer: ${computerPoints.toString()}`;
+}
 
 function play(playerSelection, computerSelection) {
     
-    // console.log(`You: ${playerSelection}`);
-    // console.log(`You: ${computerSelection}`);
-
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
+    
+    if (playerPoints === 5 || computerPoints === 5) {
+        playerPoints = 0;
+        computerPoints = 0;
+    }
     
     if (playerSelection == computerSelection) {
         return "It's tie";
     } else if (playerSelection == "ROCK") {
         switch (computerSelection) {
             case "PAPER":
+                computerPoints += 1;
+                update();
                 return "You Lose! Paper beats Rock";
             case "SCISSORS":
+                playerPoints += 1;
+                update();
                 return "You Win! Congratulations";
         }
     } else if (playerSelection == "PAPER") {
         switch (computerSelection) {
             case "ROCK":
+                playerPoints += 1;
+                update();
                 return "You Win! Congratulations";
             case "SCISSORS":
+                computerPoints += 1;
+                update();
                 return "You Lose! Scissors beat Paper";
         }
     } else {
         switch (computerSelection) {
             case "PAPER":
+                playerPoints += 1;
+                update();
                 return "You Win! Congratulations";
             case "ROCK":
+                computerPoints += 1;
+                update();
                 return "You Lose! Rock beats Scissors";
         }
     }
 }
-// console.log(play("paper", getComputerChoice()));
 
-function game() {
-    let i = 1;
-    while (i<6) {
-        console.log(play(player, getComputerChoice()));
-        i++;
+// let player = prompt("Choose rock, paper or scissors");
+
+const content = document.querySelector('#content');
+content.addEventListener('click', (event) => {
+    let target = event.target;
+    let playerSelection;
+    switch(target.id) {
+        case 'rock':
+            playerSelection = 'rock';
+            break;
+        case 'paper':
+            playerSelection = 'paper';
+            break;
+        case 'scissors':
+            playerSelection = 'scissors';
+            break;
     }
-}
+    // play(playerSelection, getComputerChoice());
+    console.log(play(playerSelection, getComputerChoice()));
+})
 
-let player = prompt("Choose rock, paper or scissors");
-
-game();
